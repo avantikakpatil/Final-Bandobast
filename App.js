933978c66ca113e1048f5b1bdb2d4e3dd3da1fb2
Avantika Patil <113052370+avantikakpatil@users.noreply.github.com>
2024-03-29 13:02:50 +0530

    new app

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from './Components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
