e9ce81447fe81e6b9346de45a2ea58f21ed86661
Avantika patil <avantikapatil420@gmail.com>
2024-03-30 12:06:58 +0530

    dash

--- my-react-app/Components/Dashboard.js
+++ my-react-app/Components/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Sidebar from './Sidebar';
-import MainContent from './MainContent';
+import Sidebar from './Components/Sidebar';
+import MainContent from './Components/MainContent';
 
 class Dashboard extends React.Component {
   render() {
