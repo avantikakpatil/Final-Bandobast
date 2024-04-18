9a26f8437f38d6556bed21b09f9ea4676f4a38d0
Kiran Wairagade <106908997+Kiranwairagade@users.noreply.github.com>
2024-04-18 17:21:26 +0530

    Add files via upload

---
+++ my-react-app/src/Components/ProfilePage.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import './ProfilePage.css';
+
+const UserProfile = () => {
+  return (
+    <div className="container">
+      <h1>FirstName MiddleName LastName</h1>
+
+      <div className="row">
+        <div className="col-md-4 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <div className="d-flex flex-column align-items-center text-center">
+                <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width="150" />
+                <p className="text-secondary mb-1">JobTitle</p>
+                <p className="text-muted font-size-sm">Address</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-8">
+          <div className="card mb-3">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-sm-3">
+                  <h6 className="mb-0">Full Name</h6>
+                </div>
+                <div className="col-sm-9 text-secondary">
+                  Kenneth Valdez
+                </div>
+              </div>
+              <hr />
+              <div className="row">
+                <div className="col-sm-3">
+                  <h6 className="mb-0">Email</h6>
+                </div>
+                <div className="col-sm-9 text-secondary">
+                  fip@jukmuh.al
+                </div>
+              </div>
+              <hr />
+              <div className="row">
+                <div className="col-sm-3">
+                  <h6 className="mb-0">Mobile</h6>
+                </div>
+                <div className="col-sm-9 text-secondary">
+                  (320) 380-4539
+                </div>
+              </div>
+              <hr />
+              <div className="row">
+                <div className="col-sm-3">
+                  <h6 className="mb-0">Address</h6>
+                </div>
+                <div className="col-sm-9 text-secondary">
+                  Bay Area, San Francisco, CA
+                </div>
+              </div>
+              <hr />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfile;
