e77cdfa31ba954e0f5fbb714b8895e2eafb7d31e
Smruti Kumbhalkar <117387914+smrutikk@users.noreply.github.com>
2024-04-24 15:30:02 +0530

    Updated App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; // Change here
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
 import Dashboard from './Components/Dashboard';
 import AddPersonnel from './Components/AddPersonnel';
+import { Login } from './Components/Login';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Switch> {/* Change here */}
-          <Route path="/" exact component={Dashboard} /> {/* Change here */}
-          <Route path="/add-personnel" component={AddPersonnel} /> {/* Change here */}
-        </Switch> {/* Change here */}
+        <Switch>
+          <Route path="/" exact component={Login} /> 
+          <Route path="/" exact component={Dashboard} /> 
+          <Route path="/add-personnel" component={AddPersonnel} /> 
+        </Switch> 
       </div>
     </Router>
   );
