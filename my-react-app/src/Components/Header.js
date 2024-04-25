aa22d32b42c86909a6937193b6ffc6b79582fdcc
Smruti Kumbhalkar <117387914+smrutikk@users.noreply.github.com>
2024-04-25 10:34:57 +0530

    Done Routing

--- my-react-app/src/Components/Header.js
+++ my-react-app/src/Components/Header.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import profileLogo from '../reshot-icon-profile-QX6KDSLJC5.svg'; // Import the profile logo
-import notificationLogo from '../reshot-icon-notification-LWSEPK97JU.svg'; // Import the notification logo
-import systemLogo from '../Bandobast system_transparent.png'; // Import the Bandobast System logo
+import profileLogo from '../reshot-icon-profile-QX6KDSLJC5.svg'; 
+import notificationLogo from '../reshot-icon-notification-LWSEPK97JU.svg'; 
+import systemLogo from '../Bandobast system_transparent.png'; 
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 class Header extends React.Component {
   render() {
     return (
       <div className="header">
         <div className="header-title">
-          <img src={systemLogo} alt="Bandobast System Logo" className="system-logo" /> {/* Bandobast System logo */}
-          Bandobast System {/* Title placed on the left */}
+          <img src={systemLogo} alt="Bandobast System Logo" className="system-logo" /> 
+          Bandobast System 
         </div>
         <div className="header-profile">
           {/* Profile logo and user name */}
           <img src={notificationLogo} alt="Notification" className="notification-icon" />
+          <Link to="/profile-page">
           <img src={profileLogo} alt="Profile" className="profile-icon" />
-          <span className="profile-name">Avantika Patil</span> {/* Replace "John Doe" with the user's name */}
+          </Link>
+          <span className="profile-name">Avantika Patil</span> 
         </div>
       </div>
     );
