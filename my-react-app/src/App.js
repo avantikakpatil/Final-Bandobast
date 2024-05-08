804b68a16e1a9aac248d76cba463273db111ee89
Avantika patil <avantikapatil420@gmail.com>
2024-05-08 20:31:48 +0530

    corrected app.js after evaluation

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
 import Dashboard from './Components/Dashboard';
-import HomepagePage from './Components/HomePage'
+import HomepagePage from './Components/HomePage';
 import AddPersonnel from './Components/AddPersonnel';
 import { Login } from './Components/Login';
-<<<<<<< HEAD
+
 import ProfilePage from './Components/ProfilePage'
-=======
-import ProfilePage from './Components/ProfilePage';
->>>>>>> 6b61b33b63f8390e4bd419544e8357af9ce2071d
+
 import HomePage from './Components/HomePage';
 
 function App() {
@@ -22,11 +20,11 @@ function App() {
           <Route path="/add-personnel" component={AddPersonnel} /> 
           <Route path="/homepage" component={HomePage} /> 
           <Route path="/dashboard" component={Dashboard} /> 
-<<<<<<< HEAD
 
-=======
+
+
           <Route path="/home-page" component={HomePage} /> 
->>>>>>> 6b61b33b63f8390e4bd419544e8357af9ce2071d
+
           <Route path="/profile-page" component={ProfilePage} />
         </Switch> 
       </div>
