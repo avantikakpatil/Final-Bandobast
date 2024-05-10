8d4f989fd86750b1a705af48ad53b6264f4a8aab
[kiran wairagade] <[kiranwairagade7@gmail.com]>
2024-05-10 09:54:50 +0530

    Additional sector information in footer

--- my-react-app/src/Components/Sidebar.js
+++ my-react-app/src/Components/Sidebar.js
@@ -1,5 +1,3 @@
-// Sidebar.js
-
 import React from 'react';
 import { ref, onValue } from 'firebase/database';
 import { db } from '../config/firebaseConfig';
@@ -12,6 +10,7 @@ class Sidebar extends React.Component {
       sectors: [],
       selectedSector: null,
       activeSectors: [], // New state to track active sectors
+      additionalInfo: null, // State to hold additional information about the selected sector
     };
   }
 
@@ -23,6 +22,7 @@ class Sidebar extends React.Component {
       if (data) {
         const sectorNames = Object.values(data).map(item => ({
           name: item.title,
+          personnel: item.personnel, // Include personnel information
           active: false // Initialize all sectors as inactive
         }));
         this.setState({ sectors: sectorNames });
@@ -32,6 +32,20 @@ class Sidebar extends React.Component {
 
   handleSectorClick = (sectorIndex) => {
     const sectors = [...this.state.sectors];
+    const selectedSector = sectors[sectorIndex];
+    this.setState({ selectedSector });
+
+    // Fetch additional information for the selected sector
+    // You can modify this based on how your data is structured
+    const additionalInfoRef = ref(db, selectedSector.title);
+    onValue(additionalInfoRef, (snapshot) => {
+      const additionalInfo = snapshot.val();
+      if (additionalInfo) {
+        this.setState({ additionalInfo });
+      }
+    });
+
+    // Toggle active state
     sectors[sectorIndex].active = !sectors[sectorIndex].active;
     this.setState({ sectors });
   }
@@ -47,8 +61,8 @@ class Sidebar extends React.Component {
             <div key={index} className={`sector ${sector.active ? 'active' : ''}`} onClick={() => this.handleSectorClick(index)}>
               <span>{sector.name}</span>
               <label className="switch rectangular">
-                <input type="checkbox" checked={sector.active} onChange={() => this.handleSectorClick(index)} />
-                <span className="slider rectangular"></span>
+                <input type="checkbox" checked={sector.active} onChange={() => {}} />
+                <span className="slider rectangular" onClick={(event) => event.stopPropagation()}></span>
               </label>
             </div>
           ))}
@@ -56,8 +70,19 @@ class Sidebar extends React.Component {
         <div className="navbar-footer">
           {this.state.selectedSector && (
             <div className="selected-sector-info">
-              <h3>{this.state.selectedSector}</h3>
-              {/* You can display additional information about the selected sector here */}
+              <h3>{this.state.selectedSector.name}</h3>
+              {this.state.additionalInfo && (
+                <div>
+                  <h4>Additional Information:</h4>
+                  <p>Title: {this.state.additionalInfo.title}</p>
+                  <p>Personnel:</p>
+                  <ul>
+                    {Object.values(this.state.additionalInfo.personnel).map((person, index) => (
+                      <li key={index}>{person.name} - {person.position}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
             </div>
           )}
         </div>
